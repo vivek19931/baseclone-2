fe3f2928dba85d0df1e843040a5f667d09a67773
vivek19931 <65123595+vivek19931@users.noreply.github.com>
2020-05-19 18:43:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
 		
     users.removeUser(socket.id);
 	
-    users.addUser(socket.id, usera, params.room);
+    users.addUser(socket.id, usera[socket.nickname], params.room);
 
     io.to(params.room).emit('updateUsersList', users.getUserList(params.room));
     socket.emit('newMessage', generateMessage('Admin', `Welocome to ${params.room}!`));
