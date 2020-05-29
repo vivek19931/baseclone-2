551d16a1458beb37db27187909629f41adf4d919
vivek19931 <65123595+vivek19931@users.noreply.github.com>
2020-05-29 15:47:12 +0530

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -244,12 +244,12 @@ document.querySelector('#submit-btn').addEventListener('click', function(e) {
   socket.emit("createMessage", {
     text: document.querySelector('input[name="message"]').value,
 	  image: document.querySelector('input[name="image"]').value,
-	  links: document.querySelector('input[name="links"]').value,
+	  
 	   space: document.querySelector('input[name="space"]').value
   }, function() {
     document.querySelector('input[name="message"]').value = '';
 	    document.querySelector('input[name="image"]').value = '';
-	  document.querySelector('input[name="links"]').value = '';
+	  
 	  document.querySelector('input[name="space"]').value = '';
 	 
   })
