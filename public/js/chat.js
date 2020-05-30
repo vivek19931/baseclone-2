a8838964b9abbe5274e842f090163004b7e9afac
vivek19931 <65123595+vivek19931@users.noreply.github.com>
2020-05-31 01:08:28 +0530

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -347,13 +347,13 @@ document.querySelector('#submit-btn').addEventListener('click', function(e) {
 
 	  
 	  
-  }), function() {
+  }function() {
     document.querySelector('input[name="message"]').value = '';
 
 	  
 	  
 	 
-  }
+  })
 })
 
 
