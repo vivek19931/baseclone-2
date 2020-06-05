3801af20f005410d69fa750eb70ee6e742e7558b
vivek19931 <65123595+vivek19931@users.noreply.github.com>
2020-06-05 13:07:55 +0530

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -436,7 +436,7 @@ document.querySelector('#submit-btn').addEventListener('click', function(e) {
 
 
   socket.on("empty", {
-    text: document.querySelector('input[name="message"]').value="";
+    text: document.querySelector('input[name="message"]').value=""
 	  
 	  
   })
